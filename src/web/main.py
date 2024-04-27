5478c823b2014837fc563326957cdc8ab3a02bf2
Ruud Verheijden <ruudverheijden@users.noreply.github.com>
2024-04-27 14:39:49 +0200

    Link face photo to display the full photo

--- src/web/main.py
+++ src/web/main.py
@@ -22,15 +22,38 @@ def faces():
     """
     db_engine = create_engine('sqlite:///' + os.environ["DATABASE_PATH"])
     with db_engine.connect() as conn:
-        query = select(faces_table.c.id, faces_table.c.thumbnail_filename)
+        query = select(
+            faces_table.c.id.label("face_id"),
+            faces_table.c.thumbnail_filename,
+            files_table.c.id.label("file_id")).join(
+                files_table,
+                faces_table.c.file_id == files_table.c.id
+            )
         result = conn.execute(query)
-        thumbnails = [{'thumbnail_path': "/thumbnails/" + row[1], 'id': row[0]} for row in result]
+        thumbnails = [{
+            'thumbnail_path': "/thumbnail/" + row.thumbnail_filename,
+            'face_id': row.face_id,
+            'file_id': row.file_id
+            } for row in result]
         conn.close()
     return render_template('faces_overview.html', thumbnails=thumbnails)
 
-@app.route("/thumbnails/<path:filename>")
-def serve_thumbnail(filename):
+@app.route("/thumbnail/<path:filename>")
+def thumbnail(filename):
     """
     Serve a thumbnail from the thumbnail folder
     """
     return send_from_directory(os.path.join("../../", os.environ["THUMBNAILS_PATH"]), filename)
+
+@app.route("/file/<int:id>")
+def file(id):
+    """
+    Show the photo from the files table based on the id
+    """
+    db_engine = create_engine('sqlite:///' + os.environ["DATABASE_PATH"])
+    with db_engine.connect() as conn:
+        query = select(files_table.c.thumbnail_filename).where(files_table.c.id == id)
+        result = conn.execute(query)
+        row = result.fetchone()
+        conn.close()
+    return render_template('file.html', file_path="/thumbnail/" + row.thumbnail_filename)
