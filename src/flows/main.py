bf085ef1b4d676995089ea3522e571fb0a17d0de
Ruud Verheijden <ruudverheijden@users.noreply.github.com>
2024-04-27 13:54:25 +0200

    Moving main.py to flows dir

--- src/flows/main.py
+++ src/flows/main.py
@@ -1,5 +1,5 @@
 from prefect import flow
-from .flows import initialize_database, parse_modified_files, generate_embeddings, generate_face_thumbnails
+from . import initialize_database, parse_modified_files, generate_embeddings, generate_face_thumbnails
 
 @flow(log_prints=True)
 def run_pipeline():
