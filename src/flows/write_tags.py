f002c41d81e52fa865c3fa0b89be2594ebae71e5
Ruud Verheijden <ruudverheijden@users.noreply.github.com>
2024-09-19 22:56:41 +0200

    Writting XMP Subject tag with names back to original files

--- src/flows/write_tags.py
+++ src/flows/write_tags.py
@@ -6,6 +6,7 @@ from dotenv import load_dotenv
 from prefect import flow, task
 from sqlalchemy import create_engine, select, outerjoin
 from exiftool import ExifToolHelper
+from collections import defaultdict
 
 from src.utils.tables import files as files_table, persons as persons_table, faces as faces_table
 
@@ -20,20 +21,33 @@ def write_tags():
     db_engine = create_engine("sqlite:///" + os.environ["DATABASE_PATH"])
     
     with db_engine.connect() as conn:
+        # Find all files with confirmed tagged persons
         query = select(
             files_table.c.path,
             persons_table.c.name
         ).select_from(
             outerjoin(
-                outerjoin(files_table, faces_table, files_table.c.id == faces_table.c.file_id),
-                persons_table, faces_table.c.person_id == persons_table.c.id
+            outerjoin(files_table, faces_table, files_table.c.id == faces_table.c.file_id),
+            persons_table, faces_table.c.person_id == persons_table.c.id
             )
-        ).where(faces_table.c.person_id.isnot(None))
+        ).where(faces_table.c.person_id.isnot(None), faces_table.c.person_id != 0)
         
         results = conn.execute(query)
 
+        # Group names by file path
+        tags_by_file = defaultdict(list)
         for result in results:
-            print(result)
+            file_path, person_name = result
+            tags_by_file[file_path].append(person_name)
+
+        # Write tags to files
+        with ExifToolHelper() as et:
+            for file_path, names in tags_by_file.items():
+                et.set_tags(
+                    [file_path],
+                    tags={"Subject": names}
+                )
+                print(f"Writting to {file_path} XMP Subject Tag = {', '.join(names)}")
 
 
 if __name__ == "__main__":
